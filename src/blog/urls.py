3138fc3d0ba7536504300e5f09ca77c6a1ac01f3
ucakmak02 <u.s.cakmak02@gmail.com>
2022-03-30 13:53:47 +0200

    dynamic url structure

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, article
 
 urlpatterns = [
-    path('', index, name='blog-index')
+    path('', index, name='blog-index'),
+    path('article-<int:num_article>/<str:title>/', article, name='article-dynamic')
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "blog/index.html")
+
+
+def article(request, num_article, title):
+    print(num_article, title)
+    return render(request, "blog/article.html", context={"num": num_article, "title": title})
